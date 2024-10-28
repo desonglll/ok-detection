bcb7878e594d6d7b25e2c68401c34b67d8eb73c1
mikeshinoda <53910940+desonglll@users.noreply.github.com>
2024-10-28 21:01:41 +0800

    First Commit.
    The dataset is very lack. Using Label Studio.

---
+++ main.py
@@ -0,0 +1,12 @@
+from ultralytics import YOLO
+
+
+def main():
+    model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
+
+    results = model.train(data="datasets/data.yaml", epochs=100, imgsz=640, device="mps")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
